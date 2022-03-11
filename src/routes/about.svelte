debf7e26e46349f194f99a91ea5bbfa6249e385a
Madmats14 <hd.cuevas@yahoo.com.ar>
2022-03-11 15:45:45 -0300

    First commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+    <title>About Svelte Pokedex</title>
+</svelte:head>
+
+<h1>About</h1>
+<p>This is a cool Svelte Kit demo</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { pokemon } from "../stores/pokestore.js";
+    
+</script>
+
+<svelte:head>
+    <title>Pokedex in Svelte</title>
+</svelte:head>
+<h1>Svelte Kit Pokedex</h1>
+
+{#each $pokemon as poke}
+<p>{poke.name}</p>
+{/each}
